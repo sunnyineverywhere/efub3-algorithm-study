2736988ec3e886f11512a5e05b3ac1e22ff391b9
Gayeong Park <maroony55@gmail.com>
2023-11-20 03:14:55 +0900

    [8주차] 표현 가능한 이진트리(재)
    - 저장이 안 된 파일이 커밋되어서 재커밋

--- 박가영/8주차/표현 가능한 이진트리.py
+++ 박가영/8주차/표현 가능한 이진트리.py
@@ -1,34 +1,51 @@
-# def makeBinaryNumberList(number):
-#     binaryNumberList = getBinaryNum(number, [])
-#     binaryNumberList.sort(reverse=True)
-#     print(binaryNumberList)
 
-# def getBinaryNum(n, numberList):
-#     a = n // 2
-#     b = n % 2
-#     numberList.append(b)
-#     if a == 0:
-#         return numberList
-#     else:
-#         return getBinaryNum(a, numberList)
+def getBinaryString(n):
+    binaryNumList = []
+    while n != 1:
+        binaryNumList.append(str(n % 2))
+        n //= 2
+    binaryNumList.append("1")
+    binaryString = ''.join(binaryNumList[::-1])
+    return binaryString
+    
+    
+def makeBinaryTree(number):
+    binaryString = getBinaryString(number)
 
-# answerList = []
-# answer = getBinaryNum(58,answerList)
-# answer.sort(reverse=True)
+    treeSize = 1
+    while treeSize < len(binaryString):
+        treeSize = (treeSize +1 ) * 2 -1
+    binaryString = "0" * (treeSize - len(binaryString)) + binaryString
+    return binaryString
 
-# print(answer)
+def checkBinaryTreePossible(start, end, binaryString):
+    if start == end:
+        return binaryString[start]
 
-def getBinaryNum(n, lists):
-    a = n // 2
-    b = n % 2
-    lists.append(b)
-    if a == 1 :
-        return lists
-    else :
-        return getBinaryNum(a, lists)
+    mid = (start + end) // 2
+    left = checkBinaryTreePossible(start, mid-1, binaryString)
+    if not left or (binaryString[mid] == "0" and left == "1"):
+        return False
 
-answerList = []
-answer = getBinaryNum(58,answerList)
-answer.sort(reverse=True)
+    right = checkBinaryTreePossible(mid+1, end, binaryString)
+    if not right or (binaryString[mid] == "0" and right == "1"):
+        return False
 
-print("".join([str(_) for _ in answer]))
+    if left == "0" and right == "0" and binaryString[mid] == "0":
+        return "0"
+
+    return "1"
+    
+
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        binaryString = makeBinaryTree(number)
+        isPossible = checkBinaryTreePossible(0, len(binaryString)-1, binaryString)
+    
+        if isPossible:
+            answer.append(1)
+        else:
+            answer.append(0)
+
+    return answer
